bcdb437c03779a47abbbda111f3525e2e73a6fa6
Teclila <teclatriff@gmail.com>
2022-05-11 20:11:15 +0200

    Update main.m

--- main.m
+++ main.m
@@ -26,10 +26,10 @@ height = 500;               % must change with hugo
 addpath(genpath('Mars_Landform'))
 n_days = 20;
 Orbit_parameters_polar = Orbit_func (FOV, height, 'polar', n_days)
-%Orbit_parameters_sunsyncnoecl = Orbit_func (FOV, height, 'sun-sync-noecl', n_days)
-%Orbit_parameters_sunsyncecl = Orbit_func (FOV, height, 'sun-sync-ecl', n_days)
+Orbit_parameters_sunsyncnoecl = Orbit_func (FOV, height, 'sun-sync-noecl', n_days)
+Orbit_parameters_sunsyncecl = Orbit_func (FOV, height, 'sun-sync-ecl', n_days)
 
-swath_deg = FOV/(Orbit_parameters_sunsyncecl.kep_el(1)-astroConstants(24))*astroConstants(24);
+visibility_deg = FOV/(Orbit_parameters_sunsyncecl.kep_el(1)-astroConstants(24))*astroConstants(24);
 
 %%
 [TCold, THot, alphaEpsilon] = ThermalDesign(Orbit, Mission, A, minRequirements, maxRequirements, Q, 1);
